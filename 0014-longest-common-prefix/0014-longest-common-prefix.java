b19c45ac5386d38b7687a48afbc04addc5cedd02
MANI S <114629537+Mani-26@users.noreply.github.com>
2024-11-30 17:41:32 +0530

    Time: 1 ms (65.4%), Space: 41.6 MB (36.6%)

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,10 +1,49 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord = false;
+    int count = 0;
+}
 class Solution {
+    TrieNode root;
+    public Solution() {
+        root = new TrieNode();
+    }
+    private void insert(String word) {
+        TrieNode current = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+            current.count++;
+        }
+        current.isEndOfWord = true;
+    }
+
+    private String findLongest(int totalWords) {
+        StringBuilder out = new StringBuilder();
+        TrieNode current = root;
+        while (true) {
+            int index = -1;
+            for (int i = 0; i < 26; i++) {
+                if (current.children[i] != null && current.children[i].count == totalWords) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1) break;
+            out.append((char) (index + 'a'));
+            current = current.children[index];
+        }
+        return out.toString();
+    }
+
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        // String prefix = strs[0];
-        for (String s : strs)
-            while (s.indexOf(strs[0]) != 0)
-                strs[0] = strs[0].substring(0, strs[0].length() - 1);
-        return strs[0];
+        for (String word : strs) {
+            insert(word);
+        }
+        return findLongest(strs.length);
     }
 }
