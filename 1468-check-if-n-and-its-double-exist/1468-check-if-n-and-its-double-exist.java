7947cf6e04061f44f4b20782f9dc028276b4f645
MANI S <114629537+Mani-26@users.noreply.github.com>
2024-12-01 16:43:17 +0530

    Time: 2 ms (93.52%), Space: 42.2 MB (97.46%)

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (i != j && arr[i] == 2 * arr[j])
-                    return true;
+        Set<Integer> hs = new HashSet<>();
+        for (int num : arr) {
+            if (hs.contains(2 * num) || (num % 2 == 0 && hs.contains(num / 2))) {
+                return true;
             }
+            hs.add(num);
         }
         return false;
     }
