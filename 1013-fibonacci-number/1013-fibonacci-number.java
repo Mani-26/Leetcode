0cc3861529eaad929975ff0d9cbf9baf151fa001
MANI S <114629537+Mani-26@users.noreply.github.com>
2024-12-02 21:17:36 +0530

    Time: 0 ms (100%), Space: 40.6 MB (13.86%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        int fibArray[] = new int[n + 1];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+        return fibArray[n];
+    }
+}
