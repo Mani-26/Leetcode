ba152b4620267bfdb05feea972833a64f9509b5f
MANI S <114629537+Mani-26@users.noreply.github.com>
2024-12-02 19:00:12 +0530

    Time: 2 ms (87.1%), Space: 40.2 MB (62.82%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,10 +1,15 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num==1) return false;
-        int val=1;
-        for(int i=2;i<=num/2;i++){
-            if(num%i==0) val+=i;
+        if (num <= 1) return false;
+        int val = 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                val += i;
+                if (i != num / i) { 
+                    val += num / i;
+                }
+            }
         }
-        return val==num;
+        return val == num;
     }
 }
