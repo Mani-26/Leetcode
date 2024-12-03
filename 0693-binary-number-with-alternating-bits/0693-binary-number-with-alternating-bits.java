66b47d31c5b214ddf32576c3943c514ce21d19ff
MANI S <114629537+Mani-26@users.noreply.github.com>
2024-12-03 19:21:13 +0530

    Time: 0 ms (100%), Space: 40.7 MB (21.67%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int pre=n&1;
+        n>>=1;
+        while(n>0){
+            if(pre==(n&1)) return false;
+            pre=n&1;
+            n>>=1;
+        }
+        return true;
+    }
+}
