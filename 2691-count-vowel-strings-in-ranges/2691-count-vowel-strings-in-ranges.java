330496ede7db752c92d9803e936e957ed037f0af
MANI S <114629537+Mani-26@users.noreply.github.com>
2025-01-02 11:42:21 +0530

    Time: 8 ms (48.29%), Space: 85.8 MB (63.33%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int n=words.length;
+        int m=queries.length;
         int arr[]=new int[n];
         int prefix[]=new int[n];
-        int m=queries.length;
         int res[]=new int[m];
         HashSet<Character> hs=new HashSet<>();
         hs.add('a');hs.add('e');hs.add('i');hs.add('o');hs.add('u');
         for(int i=0;i<n;i++){
             int k=words[i].length()-1;
-            // if((words[i].charAt(0)=='a'||words[i].charAt(0)=='e'||words[i].charAt(0)=='i'||words[i].charAt(0)=='o'||words[i].charAt(0)=='u')&&(words[i].charAt(k)=='a'||words[i].charAt(k)=='e'||words[i].charAt(k)=='i'||words[i].charAt(k)=='o'||words[i].charAt(k)=='u')){
             if(hs.contains(words[i].charAt(0))&&hs.contains(words[i].charAt(k))){
                 arr[i]=1;
             }
@@ -19,28 +18,15 @@ class Solution {
             prefix[i] = prefix[i - 1] + arr[i];
         }
         for(int i=0;i<m;i++){
-            if(queries[i][0]==0){
-                res[i]=prefix[queries[i][1]];
+            int st=queries[i][0];
+            int end=queries[i][1];
+            if(st==0){
+                res[i]=prefix[end];
             }
-            // else if(queries[i][0]==queries[i][1]){
-            //     res[i]=arr[queries[i][1]];
-            // }
             else{
-                res[i]=prefix[queries[i][1]]-prefix[queries[i][0]-1];
+                res[i]=prefix[end]-prefix[st-1];
             }
-            // res[i]=(queries[i][0]!=0)?arr[queries[i][1]-1]-arr[queries[i][0]]:arr[queries[i][1]];
         }
-        // for(int i=0;i<m;i++){
-        //     int count=0;
-        //     for(int j=queries[i][0];j<=queries[i][1];j++){
-        //         if(arr[j]){
-        //             count++;
-        //         }
-        //     }
-        //     res[i]=count;
-        // }
-        // System.out.println(Arrays.toString(prefix));
-        // System.out.println(Arrays.toString(res));
         return res;
     }
 }
