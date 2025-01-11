342c2e4ec47d5f95cbe6ae32ca317bc6b6d97eab
MANI S <114629537+Mani-26@users.noreply.github.com>
2025-01-11 08:31:14 +0530

    Time: 4 ms (94.6%), Space: 45.2 MB (84.53%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean canConstruct(String s, int k) {
         int n=s.length();
         if(k>n) return false;
-        if(k==n) return true;
         int freq[]=new int[26];
         int oddCount=0;
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+        for(int i=0;i<n;i++){
+            freq[s.charAt(i)-'a']++;
         }
-        for (int count : freq) {
-            if (count % 2 == 1) {
-               oddCount++;
+        for(int i=0;i<26;i++){
+            if(freq[i]%2==1){
+                oddCount++;
             }
         }
         if(oddCount>k) return false;
