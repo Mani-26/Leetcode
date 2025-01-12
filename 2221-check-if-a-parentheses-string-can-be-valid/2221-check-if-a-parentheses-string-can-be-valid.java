b8f3a6641794ed226204a4e94141867de95492e5
MANI S <114629537+Mani-26@users.noreply.github.com>
2025-01-12 23:17:18 +0530

    Time: 14 ms (86.72%), Space: 44.9 MB (100%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -4,33 +4,27 @@ class Solution {
         if(n%2==1){
             return false;
         }
-        int wild=0,open=0,close=0;
+        int open=0,close=0;
         for(int i=0;i<n;i++){
-            if(locked.charAt(i)=='0'){
-                wild++;
-            }
-            else if(s.charAt(i)=='('){
+            if(locked.charAt(i)=='0'||s.charAt(i)=='('){
                 open++;
             }
             else{
                 close++;
             }
-            if(wild+open<close){
+            if(open<close){
                 return false;
             }
         }
-        wild=open=close=0;
+        open=close=0;
         for(int i=n-1;i>=0;i--){
-            if(locked.charAt(i)=='0'){
-                wild++;
-            }
-            else if(s.charAt(i)=='('){
-                open++;
-            }
-            else{
+            if(locked.charAt(i)=='0'||s.charAt(i)==')'){
                 close++;
             }
-            if(wild+close<open){
+            else{
+                open++;
+            }
+            if(close<open){
                 return false;
             }
         }
