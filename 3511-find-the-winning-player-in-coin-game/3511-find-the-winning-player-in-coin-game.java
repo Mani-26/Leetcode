b9da68e906b3d54f8a2c7555c40f2fea129bb8b5
MANI S <114629537+Mani-26@users.noreply.github.com>
2025-01-26 19:09:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (16.38%) - LeetHub

--- 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.java
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.java
@@ -1,11 +1,5 @@
 class Solution {
     public String winningPlayer(int x, int y) {
-        int count=0;
-        while(x>=1&&y>=4){
-            count++;
-            x--;
-            y-=4;
-        }
-        return (count%2==1)?"Alice":"Bob";
+        return (Math.min(x,y/4)%2==1)?"Alice":"Bob";
     }
 }
