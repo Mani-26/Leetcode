452ebe0a4c6ef5f7756d9a9256c2ccc18b667aa1
MANI S <114629537+Mani-26@users.noreply.github.com>
2025-07-21 18:05:29 +0530

    Time: 1 ms (98.6%), Space: 42 MB (77.23%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean isValid(String word) {
-        boolean min = word.length() >= 3;
-        boolean valid = true;
-        boolean vowel = word.replaceAll("[aeiouAEIOU]", "").length() != word.length();
-        boolean consonant = word.replaceAll("[^aeiouAEIOU1-9]", "").length() != word.length();
-        for (int i : word.toCharArray()) {
-            if (!((i >= '0' && i <= '9') || (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
-                valid = false;
+        if (word.length() < 3) {
+            return false;
+        }
+        int vol = 0, con = 0;
+        for (int i = 0; i<word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))) {
+                return false;
+            }
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                    || ch == 'O' || ch == 'U') {
+                vol++;
+            } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                con++;
             }
         }
-        System.out.print(word.replaceAll("[^aeiouAEIOU]", ""));
-        return min && valid && vowel && consonant;
+        return vol > 0 && con > 0;
     }
 }
