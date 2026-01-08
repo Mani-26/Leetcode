be6cf5564cf525f79c20b6a799a60b69c1258ca7
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-01-08 17:14:56 +0530

    Time: 0 ms (100%), Space: 47.4 MB (51.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        boolean[] arr=new boolean[nums.length+1];
-        for(int i: nums){
-            arr[i]=true;
+        int n = nums.length;
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
         }
-        int i=0;
-        for(i=0;i<arr.length;i++){
-            if(!arr[i]) return i;
-        }
-        return i;
+        return n * (n + 1) / 2 - sum;
     }
 }
