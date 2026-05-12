2f9ce8ea5e4003b4cf342f97b45bfb1a97ddd66a
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-05-12 19:54:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,8 +15,24 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null) return true;
-        if(p==null||q==null) return false;
-        return (p.val==q.val)&& isSameTree(p.left,q.left) &&isSameTree(p.right,q.right);
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.offer(p);
+        q2.offer(q);
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode c1 = q1.poll();
+            TreeNode c2 = q2.poll();
+            if ((c1 != null && c2 == null) || (c1 == null && c2 != null) || (c1 != null && c2 != null &&c1.val != c2.val)) {
+                return false;
+            }
+            if (c1 != null && c2 != null) {
+
+                q1.offer(c1.left);
+                q1.offer(c1.right);
+                q2.offer(c2.left);
+                q2.offer(c2.right);
+            }
+        }
+        return true;
     }
 }
