11aa95a78f3172c4a68acfb1a3d1e2fbd7265c45
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-07-01 22:16:18 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,49 +1,11 @@
-class TrieNode {
-    TrieNode[] children = new TrieNode[26];
-    boolean isEndOfWord = false;
-    int count = 0;
-}
 class Solution {
-    TrieNode root;
-    public Solution() {
-        root = new TrieNode();
-    }
-    private void insert(String word) {
-        TrieNode current = root;
-        for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (current.children[index] == null) {
-                current.children[index] = new TrieNode();
-            }
-            current = current.children[index];
-            current.count++;
-        }
-        current.isEndOfWord = true;
-    }
-
-    private String findLongest(int totalWords) {
-        StringBuilder out = new StringBuilder();
-        TrieNode current = root;
-        while (true) {
-            int index = -1;
-            for (int i = 0; i < 26; i++) {
-                if (current.children[i] != null && current.children[i].count == totalWords) {
-                    index = i;
-                    break;
-                }
-            }
-            if (index == -1) break;
-            out.append((char) (index + 'a'));
-            current = current.children[index];
-        }
-        return out.toString();
-    }
-
     public String longestCommonPrefix(String[] strs) {
+        // Arrays.sort(strs);
         if (strs == null || strs.length == 0) return "";
-        for (String word : strs) {
-            insert(word);
-        }
-        return findLongest(strs.length);
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
     }
 }
