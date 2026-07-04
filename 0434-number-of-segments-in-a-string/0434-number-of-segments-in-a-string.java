548c348dd7ea0ab5df3bd4c17e83880d9525c4d6
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-07-04 15:26:16 +0530

    Time: 0 ms (100%), Space: 42.5 MB (70.25%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,13 +1,22 @@
 class Solution {
     public int countSegments(String s) {
-        int count = 0;
+        // return s.trim().split("\\s+").length;
 
-        for (int i = 0; i < s.length(); i++) {
-            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+        // String arr[]=s.split(" ");
+        // int count=0;
+        // for(String str:arr){
+        //     if(str.length()>0) count++;
+        // }
+        // return count;
+
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(i==0 && s.charAt(i)!=' '){
+                count++;
+            }else if(s.charAt(i)!=' ' && s.charAt(i-1)==' '){
                 count++;
             }
         }
-
         return count;
     }
 }
