45f38ec29f2f9d6f4f3f29c8a2b23bf69bd09e08
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-07-09 00:00:52 +0530

    Time: 10 ms (72.63%), Space: 47.6 MB (55.76%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n=score.length;
+        int arr[][]=new int[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=score[i];
+            arr[i][1]=i;
+        }
+        Arrays.sort(arr,(a,b)->b[0]-a[0]);
+        String res[]=new String[n];
+        for(int i=0;i<n;i++){
+            int index=arr[i][1];
+            if(i==0){
+                res[index]="Gold Medal";
+            }else if(i==1){
+                res[index]="Silver Medal";
+            }else if(i==2){
+                res[index]="Bronze Medal";
+            }else{
+                res[index]=String.valueOf(i+1);
+            }
+        }
+        return res;
+    }
+}
