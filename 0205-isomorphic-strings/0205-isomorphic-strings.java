d75f2019dc88593137cab7c9e2feeec0c8dc462c
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-07-13 08:03:53 +0530

    Time: 16 ms (46.01%), Space: 44.1 MB (15.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        Map<Character,Character> mp1=new HashMap<>();
-        Map<Character,Character> mp2=new HashMap<>();
-        StringBuilder sb1=new StringBuilder();
-        StringBuilder sb2=new StringBuilder();
-        for(int i=0,j=0;i<s.length();i++,j++){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(i);
-            if(!mp1.containsKey(ch1)){
-                mp1.put(ch1,ch2);
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                mapST.put(c1, c2);
             }
-            sb1.append(mp1.get(ch1));
-            ch1=t.charAt(j);
-            ch2=s.charAt(j);
-            if(!mp2.containsKey(ch1)){
-                mp2.put(ch1,ch2);
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1) {
+                    return false;
+                }
+            } else {
+                mapTS.put(c2, c1);
             }
-            sb2.append(mp2.get(ch1));
         }
-        return sb1.toString().equals(t)&&sb2.toString().equals(s);
-        // Set<Character> sSet=new HashSet<>();
-        // Set<Character> tSet=new HashSet<>();
-        // for(int i=0;i<s.length();i++) sSet.add(s.charAt(i));
-        // for(int i=0;i<t.length();i++) tSet.add(t.charAt(i));
-        // return sSet.size()==tSet.size();
+        return true;
     }
 }
