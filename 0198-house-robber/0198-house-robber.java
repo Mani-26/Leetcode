05d885661c1808503e51872b848257dc75876c4f
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-07-14 10:28:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int next1=0;
+        int next2=0;
+        for(int i=nums.length-1;i>=0;i--){
+            int current=Math.max(nums[i]+next2 , next1);
+            next2=next1;
+            next1=current;
+        }
+        return next1;
+    }
+}
