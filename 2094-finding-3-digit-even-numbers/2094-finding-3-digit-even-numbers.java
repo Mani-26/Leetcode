d0f6fcf898ae593bfdca8dfc5d6b94d420b45fcb
MANI S <114629537+Mani-26@users.noreply.github.com>
2026-09-11 18:45:24 +0530

    Time: 7 ms (77.87%), Space: 46.6 MB (55.42%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -0,0 +1,47 @@
+class Solution {
+    int n;
+    int digits[];
+    ArrayList<Integer> li = new ArrayList<>();
+
+    public int[] findEvenNumbers(int[] digits) {
+        n = digits.length;
+        this.digits = digits;
+        boolean[] flag = new boolean[n];
+        boolean[] used = new boolean[10];
+        for (int i = 0; i < n; i++) {
+            if (digits[i] == 0 || used[digits[i]])
+                continue;
+            used[digits[i]] = true;
+            flag[i] = true;
+            make3Digit(digits[i], flag);
+            flag[i] = false;
+        }
+
+        Collections.sort(li);
+        int[] ans = new int[li.size()];
+
+        for (int i = 0; i < li.size(); i++) {
+            ans[i] = li.get(i);
+        }
+
+        return ans;
+
+    }
+
+    void make3Digit(int num, boolean[] flag) {
+        if (num >= 100) {
+            if (num % 2 == 0)
+                li.add(num);
+            return;
+        }
+        boolean[] used = new boolean[10];
+        for (int j = 0; j < n; j++) {
+            if (flag[j] || used[digits[j]])
+                continue;
+            used[digits[j]] = true;
+            flag[j] = true;
+            make3Digit(num * 10 + digits[j], flag);
+            flag[j] = false;
+        }
+    }
+}
